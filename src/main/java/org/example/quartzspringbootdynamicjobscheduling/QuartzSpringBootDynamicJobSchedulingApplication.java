e21881de7aa5cccb46c72da77f51c3366f3b4e45
priyanthabuddhika <priyanthabuddhika@gmail.com>
2023-12-21 00:06:54 +0530

    Initialize project

---
+++ src/main/java/org/example/quartzspringbootdynamicjobscheduling/QuartzSpringBootDynamicJobSchedulingApplication.java
@@ -0,0 +1,13 @@
+package org.example.quartzspringbootdynamicjobscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzSpringBootDynamicJobSchedulingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzSpringBootDynamicJobSchedulingApplication.class, args);
+    }
+
+}
